41192e3af2c1db97e097a5068144b9aaa12a8ede
Hunter Downey <hdowneymac@gmail.com>
2022-06-28 23:38:29 -0500

    Remove From Playlist Functionality

--- Vibratio/Controllers/Core/Library/LibraryPlaylistsViewController.swift
+++ Vibratio/Controllers/Core/Library/LibraryPlaylistsViewController.swift
@@ -152,6 +152,7 @@ extension LibraryPlaylistsViewController: UITableViewDelegate, UITableViewDataSo
         
         let vc = PlaylistViewController(playlist: playlist)
         vc.navigationItem.largeTitleDisplayMode = .never
+        vc.isOwner = true
         navigationController?.pushViewController(vc, animated: true)
     }
     
--- Vibratio/Controllers/Other/PlaylistViewController.swift
+++ Vibratio/Controllers/Other/PlaylistViewController.swift
@@ -11,6 +11,8 @@ class PlaylistViewController: UIViewController {
     
     private let playlist: Playlist
     
+    public var isOwner = false
+    
     private let collectionView = UICollectionView(
         frame: .zero,
         collectionViewLayout: UICollectionViewCompositionalLayout(sectionProvider: { _, _ -> NSCollectionLayoutSection? in
@@ -100,6 +102,45 @@ class PlaylistViewController: UIViewController {
             action: #selector(didTapShare)
         )
         
+        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(didLongPress(_:)))
+        collectionView.addGestureRecognizer(gesture)
+    }
+    
+    @objc func didLongPress(_ gesture: UILongPressGestureRecognizer) {
+        guard gesture.state == .began else {
+            return
+        }
+        
+        let touchPoint = gesture.location(in: collectionView)
+        
+        guard let indexPath = collectionView.indexPathForItem(at: touchPoint) else {
+            return
+        }
+        
+        let trackToDelete = tracks[indexPath.row]
+        
+        let actionSheet = UIAlertController(
+            title: trackToDelete.name,
+            message: "Would you like to remove this from the playlist?",
+            preferredStyle: .actionSheet
+        )
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Remove", style: .destructive, handler: { [weak self] _ in
+            guard let strongSelf = self else {
+                return
+            }
+            APICaller.shared.removeTrackFromPlaylist(track: trackToDelete, playlist: strongSelf.playlist) { success in
+                DispatchQueue.main.async {
+                    if success {
+                        strongSelf.tracks.remove(at: indexPath.row)
+                        strongSelf.viewModels.remove(at: indexPath.row)
+                        strongSelf.collectionView.reloadData()
+                    }
+                }
+            }
+        }))
+        
+        present(actionSheet, animated: true, completion: nil)
     }
     
     @objc private func didTapShare() {
